e90984ed2849c73c8722620e1f58bec17d41ea62
srinadh1304 <srinadh1304@gmail.com>
2021-10-12 23:33:10 +0530

    MODIFIED: method to store employee payroll object to local storage

--- js/EmployeePayrollService.js
+++ js/EmployeePayrollService.js
@@ -1,9 +1,20 @@
 const save = () => {
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }catch(e){
         return;
     }
+}
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(employeePayrollData);
+    }else{
+        employeePayrollList = [employeePayrollData]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
 }
 
 const createEmployeePayroll = () => {
