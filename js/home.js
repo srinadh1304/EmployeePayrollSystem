f9f74db63ba2669227e8562147f084af9433eabf
srinadh1304 <srinadh1304@gmail.com>
2021-10-14 06:31:08 +0530

    MODIFIED: table header is dynamically added

--- js/home.js
+++ js/home.js
@@ -3,16 +3,9 @@ window.addEventListener('DOMContentLoaded',(event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-        <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Department</th>
-            <th>Salary</th>
-            <th>Start Date</th>
-            <th>Actions</th>
-        </tr>
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    const innerHtml = `${headerHtml}
         <tr>
             <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"></td>
             <td>Srinadh T</td>
@@ -21,7 +14,7 @@ const createInnerHtml = () => {
                 <div class="dept-label">Finance</div>
             </td>
             <td>500000</td>
-            <td>1 Nov 2020</td>
+            <td>13 Oct 2021</td>
             <td>
                 <img id="1" onclick="remove(this)" alt="delete"
                     src="../assets/icons/delete-black-18dp.svg">
