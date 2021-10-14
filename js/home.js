6afdaf9f7f8161a060620840fc8edb25c9816067
srinadh1304 <srinadh1304@gmail.com>
2021-10-14 06:59:47 +0530

    ADDED: display all employee details using for loop

--- js/home.js
+++ js/home.js
@@ -5,15 +5,15 @@ window.addEventListener('DOMContentLoaded',(event) => {
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let employeePayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = `${headerHtml}
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const employeePayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" alt="" src="${employeePayrollData._profilePic}" alt=""></td>
             <td>${employeePayrollData._name}</td>
             <td>${employeePayrollData._gender}</td>
-            <td><div class="dept-label">${employeePayrollData._department[0]}</div>
-                <div class="dept-label">${employeePayrollData._department[1]}</div>
-            </td>
+            <td>${getDeptHtml(employeePayrollData._department)}</td>
             <td>${employeePayrollData._salary}</td>
             <td>${employeePayrollData._startDate}</td>
             <td>
@@ -24,12 +24,21 @@ const createInnerHtml = () => {
             </td>
         </tr>
     `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
 const createEmployeePayrollJSON = () => {
     let empPayrollListLocal = [
         {
-            _name: 'Sheshadri Bhat',
+            _name: 'Srinadh',
             _gender: 'Male',
             _department: [
                 'HR',
@@ -42,16 +51,16 @@ const createEmployeePayrollJSON = () => {
             _profilePic: '../assets/profile-images/Ellipse -2.png'
         },
         {
-            _name: 'Smitha Naveen',
+            _name: 'Lavanya',
             _gender: 'Female',
             _department: [
                 'Finance'
             ],
             _salary: '400000',
-            _startDate: '3 Oct 2021',
+            _startDate: '13 Oct 2021',
             _note: '',
             _id: new Date().getTime() + 1,
-            _profilePic: '../assets/profile-images/Ellipse -4.png'
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
         }
     ];
     return empPayrollListLocal;
