c98b41fdb6179c2170e405b7653c7b00d9ded2cb
srinadh1304 <srinadh1304@gmail.com>
2021-10-15 21:38:12 +0530

    REFACTORED: given proper indentation

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,4 @@
-let site_properties=
-{
-    home_page:"../pages/home.html",
-    add_emp_payroll_page:"../pages/new_payroll_Form"
-}
+let site_properties = {
+    home_page: "../pages/home.html",
+    add_emp_payroll_page: "../pages/payroll_form.html"
+};
