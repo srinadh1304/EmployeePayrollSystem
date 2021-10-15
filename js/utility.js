a7dfa90c07cf7e003f81ea764b5e86bcb765afa8
srinadh1304 <srinadh1304@gmail.com>
2021-10-15 21:41:29 +0530

    MODIFIED: update method to update employee details

--- js/utility.js
+++ js/utility.js
@@ -4,3 +4,10 @@ const stringifyDate = (date) => {
                     new Date(Date.parse(date)).toLocaleDateString('en-GB',options);
     return newDate;
 }
+
+const update = (node) => {
+    let employeePayrollData = employeePayrollList.find(empData => empData._id == node.id);
+    if(!employeePayrollData) return;
+    localStorage.setItem('editEmp',JSON.stringify(employeePayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
