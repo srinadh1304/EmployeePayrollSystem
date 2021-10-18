a456f0fd3f6082a4660c0ad43a915fd0b53c1b55
srinadh1304 <srinadh1304@gmail.com>
2021-10-18 23:21:48 +0530

    ADDED: new site properties

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,4 +1,6 @@
 let site_properties = {
+    use_local_storage: "true",
     home_page: "../pages/home.html",
+    server_url: "http://localhost:3000/EmployeePayrollDB/",
     add_emp_payroll_page: "../pages/payroll_form.html"
 };
