f06ddfc09be31c37c66b215d2930027f6278a606
srinadh1304 <srinadh1304@gmail.com>
2021-10-18 23:27:56 +0530

    REFACTORED: use_local_storage attribute is set to false

--- js/site_properties.js
+++ js/site_properties.js
@@ -1,5 +1,5 @@
 let site_properties = {
-    use_local_storage: "true",
+    use_local_storage: "false",
     home_page: "../pages/home.html",
     server_url: "http://localhost:3000/EmployeePayrollDB/",
     add_emp_payroll_page: "../pages/payroll_form.html"
