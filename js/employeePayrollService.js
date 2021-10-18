564b7e7305bd85ee2181267aedc28cdd4874f189
srinadh1304 <srinadh1304@gmail.com>
2021-10-18 23:32:50 +0530

    ADDED: function to add employee data to json server

--- js/employeePayrollService.js
+++ js/employeePayrollService.js
@@ -49,12 +49,34 @@ const save = (event) => {
     event.stopPropagation();
     try {
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace(site_properties.home_page);
+        if (site_properties.use_local_storage.match("true")) {
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     } catch (e) {
+        console.log(e);
         return;
     }
+}
+
+const createOrUpdateEmployeePayroll = () => {
+    let postUrl = site_properties.server_url;
+    let methodCall = "POST";
+    if (isUpdate) {
+        methodCall = "PUT";
+        postUrl = postUrl + employeePayrollObj.id.toString();
+    }
+    makeServiceCall(methodCall, postUrl, true, employeePayrollObj)
+        .then(responceText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch(error => {
+            throw error;
+        });
 }
 
 const setEmployeePayrollObject = () => {
