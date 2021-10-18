311ff570290d8e8b53024cd8156204f95ecf35f9
srinadh1304 <srinadh1304@gmail.com>
2021-10-18 23:20:06 +0530

    ADDED: functions to validate start date and name

--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,14 @@ const stringifyDate = (date) => {
     const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
     return newDate;
 }
+
+const checkName = (name) => {
+    let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (!nameRegex.test(name)) throw 'Name is Incorrect!';
+}
+
+const checkStartDate = (startDate) => {
+    let thirtyDaysInMiliSec = 30 * 24 * 60 * 60 * 1000;
+    if (!(startDate <= new Date() && Date.now() - startDate < thirtyDaysInMiliSec))
+        throw 'startDate is Incorrect';
+}
